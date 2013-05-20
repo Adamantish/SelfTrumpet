634d3f8737f6ba1415e90532ae3fdc1103d8a390
Dan Garland <dan@dangarland.co.uk>
2013-05-20 02:19:38 -0400

    Ensuring that the DOM is loaded

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,18 @@
-// Try to find projects already in the local storage
-var projectList = new app.collections.ProjectList();
-projectList.fetch();
+$(document).ready(function() {
+  // Try to find projects already in the local storage
+  var projectList = new app.collections.ProjectList();
+  projectList.fetch();
 
-// Create a dummy project if there isn't one already
-if(projectList.length == 0) {
-  var bucket_list = projectList.create({
-    title: "Bucketlist",
-    url: "https://github.com/dmgarland/BucketListApp",
-    body: "<p>I worked on a Rails application that created a todo list of things I want to do before I die.</p> <ul> <li>I integrated Google maps and used Geocoding to determine where my activities would take place.</li> <li>I used AJAX to asynchronously update markers on the map when the center changed.</li> <li>I displayed crime statistics on a chart using an API call and Morris.js</li> </ul>"
-  });
-}
+  // Create a dummy project if there isn't one already
+  if(projectList.length == 0) {
+    var bucket_list = projectList.create({
+      title: "Bucketlist",
+      url: "https://github.com/dmgarland/BucketListApp",
+      body: "<p>I worked on a Rails application that created a todo list of things I want to do before I die.</p> <ul> <li>I integrated Google maps and used Geocoding to determine where my activities would take place.</li> <li>I used AJAX to asynchronously update markers on the map when the center changed.</li> <li>I displayed crime statistics on a chart using an API call and Morris.js</li> </ul>"
+    });
+  }
 
-// Create a view for the first Project and render it
-var view = new app.views.ProjectView({ model: projectList.first() });
-$('#project-list').append(view.render().el);
+  // Create a view for the first Project and render it
+  var view = new app.views.ProjectView({ model: projectList.first() });
+  $('#project-list').append(view.render().el);
+});
