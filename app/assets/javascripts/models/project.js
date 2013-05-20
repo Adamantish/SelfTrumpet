d737f8ff6639f55fe2e363b82f4dc45d949cf3dc
Dan Garland <dan@dangarland.co.uk>
2013-05-20 14:46:29 -0400

    Added project test

--- app/assets/javascripts/models/project.js
+++ app/assets/javascripts/models/project.js
@@ -1,3 +1,19 @@
 app.models.Project = Backbone.Model.extend({
 
+  localStorage: new Backbone.LocalStorage('portfolio'),
+
+  initialize: function() {
+    this.bind("change", this.updateTitle);
+  },
+
+  updateTitle: function() {
+    this.attributes.title += " Changed";
+  },
+
+  validate: function() {
+    if(this.attributes.url === "") {
+      return "Argh!";
+    }
+  }
+
 });
--- spec/ProjectSpec.js
+++ spec/ProjectSpec.js
@@ -4,12 +4,57 @@ describe("A Project", function() {
 
   beforeEach(function() {
     project = new app.models.Project({
-      title: "My amazing test project"
+      title: "My amazing test project",
+      url: "http://example.org"
     });
   });
 
   it("should be able to retreive the title", function() {
     expect(project.get("title")).toEqual("My amazing test project");
+  });
+
+  it("should not have an id because its not persisted", function() {
+    expect(project.id).toBeUndefined();
+  });
+
+  it("should have an cid", function() {
+    expect(project.cid).not.toBe(null);
+  });
+
+  describe("Persistance in local storage", function() {
+    beforeEach(function() {
+      project.save();
+    });
+
+    it("should have an id", function() {
+      expect(project.id).not.toBe(null);
+    });
+  });
+
+  describe("Setting an attribute", function() {
+    beforeEach(function() {
+      project.set({
+        "title" : "Cool Beans"
+      });
+    });
+
+    it("Should update the title", function() {
+      expect(project.get("title")).toEqual("Cool Beans Changed");
+    });
+
+  });
+
+  describe("validation", function() {
+    beforeEach(function() {
+      project = new app.models.Project({
+        title: "My amazing test project",
+        url: ""
+      });
+    });
+
+    it("should not be valid without a URL", function() {
+      expect(project.isValid()).toBeFalsy();
+    });
   });
 
 });
