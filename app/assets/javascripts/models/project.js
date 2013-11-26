967e3f6cc94516aeccc45134a816c01bcda69190
Dan Garland <dan@dangarland.co.uk>
2013-11-26 12:08:12 +0000

    Updating backbone

--- app/assets/javascripts/models/project.js
+++ app/assets/javascripts/models/project.js
@@ -6,13 +6,17 @@ app.models.Project = Backbone.Model.extend({
     this.bind("change", this.updateTitle);
   },
 
-  updateTitle: function() {
-    this.attributes.title += " Changed";
-  },
+  // updateTitle: function() {
+  //   this.attributes.title += " Changed";
+  // },
 
   validate: function() {
     if(this.attributes.url === "") {
       return "Argh!";
+    }
+
+    if(this.attributes.title == "") {
+      return false;
     }
   }
 
