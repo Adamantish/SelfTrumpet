6bdf4724b6074b9dd3d74588263cb3a8c5234a76
Dan Garland <dan@dangarland.co.uk>
2015-12-08 15:01:49 +0000

    Tidying up validations

--- app/assets/javascripts/models/project.js
+++ app/assets/javascripts/models/project.js
@@ -3,13 +3,8 @@ app.models.Project = Backbone.Model.extend({
   localStorage: new Backbone.LocalStorage('portfolio'),
 
   validate: function() {
-    if(this.attributes.url === "" || this.attributes.url === undefined) {
-      return "URL can't be blank";
-    };
-
-    if(this.attributes.title === "" || this.attributes.title === undefined) {
-      return "Title can't be blank";
-    };
+    if(!this.attributes.url) return "URL can't be blank";
+    if(!this.attributes.title) return "Title can't be blank";
   }
 
 });
