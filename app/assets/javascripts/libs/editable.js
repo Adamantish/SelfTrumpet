d12b692e5f54a33a9ce6bda9ba6ebf75637c29ce
Adamantish <sonsequence@hotmail.com>
2015-12-09 03:10:14 +0000

    Edit box showing on element double click

--- app/assets/javascripts/libs/editable.js
+++ app/assets/javascripts/libs/editable.js
@@ -7,6 +7,10 @@ Editable.prototype.makeInputBoxes = function($el, model) {
   _.each(model.attributes, function(value, field) {
     var anchor = $el.find("." + field + ".editable");
     var editBox = template({ field: field, value: value });
+
+    // In two minds about this wrapping element. 
+    // It's only handy for toggling hidden class of the things inside.
+    anchor.wrap("<div class='container__edit'></div>")
     anchor.after(editBox);
   })
 
--- app/assets/javascripts/views/userView.js
+++ app/assets/javascripts/views/userView.js
@@ -3,10 +3,18 @@ app.views.UserView = Backbone.View.extend({
   tagName: 'div',
   className: "bio",
   template: _.template($('#user-template').html()),
+  events: {
+    'dblclick .editable': 'editMe'
+  },
 
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
+  },
+
+  editMe: function(e){
+    debugger;
+    $(e.currentTarget).parent().children().toggle()
   }
 
 });
