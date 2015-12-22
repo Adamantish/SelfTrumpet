b07f1eb9ffc33304760680937588b4cf85e4afe6
Adamantish <sonsequence@hotmail.com>
2015-12-22 18:11:12 +0000

    image data gets to server but needs handling

--- app/assets/javascripts/views/userView.js
+++ app/assets/javascripts/views/userView.js
@@ -7,10 +7,10 @@ app.views.UserView = Backbone.View.extend({
   // template: _.template($('#user-template').html()),
   events: {
     'dblclick .editable': 'toggleVisible',
+    'dblclick .uploadable': 'toggleVisible',
     'blur .user__edit-me': 'updateElement',
     'change .user__edit-me': 'updateElement',
-    'click #btn--follow': 'follow'
-
+    'change .file-input': 'uploadFile'
   },
 
   initialize: function(){
@@ -34,12 +34,37 @@ app.views.UserView = Backbone.View.extend({
     $(e.currentTarget).next().show()
   },
 
+  uploadFile: function(e){
+    
+    var editBox = $(e.currentTarget)
+  
+    var fileData = new FormData();
+    fileData.append('imageFile', editBox[0].files[0]);
+    debugger;
+
+    $.ajax({
+      beforeSend: function(xhr) {
+        var csrf_token = $('meta[name=csrf-token]').attr('content');
+        xhr.setRequestHeader('X-CSRF-Token', csrf_token );
+      },
+      url: 'users/1',
+      data: fileData,
+      processData: false,
+      contentType: false,
+      type: 'PUT'
+    })
+
+    this.model.trigger("change");
+  },
+
   updateElement: function(e){
+
     var $el = $(e.currentTarget)
     var field = $el.data("attributes")
     var newVal = $(e.currentTarget).val().trim();
     this.model.set(field, $el.val().trim());
     this.model.save();
+
   },
 
   // follow: function(){
